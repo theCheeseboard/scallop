1e2ad3263b887c4259fb9cdbba3d64ac14789db8
Victor Tran <vicr12345@gmail.com>
2020-11-25 15:23:42 +1100

    Add translations

---
+++ boot-utilities/translations/tr_TR.ts
@@ -0,0 +1,43 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="tr_TR" sourcelanguage="en_US">
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="../mainwindow.ui" line="14"/>
+        <source>MainWindow</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="70"/>
+        <source>Exit</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="84"/>
+        <location filename="../mainwindow.cpp" line="74"/>
+        <source>Install %1</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="91"/>
+        <source>Disks</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="98"/>
+        <source>Terminal</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="72"/>
+        <source>Unknown</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="73"/>
+        <source>Welcome to %1!</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
---
+++ installer/translations/ar_KM.ts
@@ -0,0 +1,428 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="km_KH" sourcelanguage="en_US">
+<context>
+    <name>BootloaderState</name>
+    <message>
+        <location filename="../install/bootloaderstate.cpp" line="37"/>
+        <source>Configuring Bootloader...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../install/bootloaderstate.cpp" line="43"/>
+        <source>Failed to create the GRUB Configuration File</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../install/bootloaderstate.cpp" line="60"/>
+        <source>Installing the bootloader...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../install/bootloaderstate.cpp" line="67"/>
+        <source>Failed to install GRUB</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>DiskManagementState</name>
+    <message>
+        <location filename="../install/diskmanagementstate.cpp" line="58"/>
+        <source>Erasing disk %1</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../install/diskmanagementstate.cpp" line="64"/>
+        <source>Failed to erase the disk</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../install/diskmanagementstate.cpp" line="73"/>
+        <source>Creating Boot Partition</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../install/diskmanagementstate.cpp" line="90"/>
+        <location filename="../install/diskmanagementstate.cpp" line="104"/>
+        <source>Failed to create the boot partition</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../install/diskmanagementstate.cpp" line="118"/>
+        <source>Creating Root Partition</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../install/diskmanagementstate.cpp" line="130"/>
+        <source>Failed to create the root partition</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>DiskPage</name>
+    <message>
+        <location filename="../pages/diskpage.ui" line="14"/>
+        <source>Form</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../pages/diskpage.ui" line="35"/>
+        <source>Where to today?</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../pages/diskpage.ui" line="85"/>
+        <source>All data on the selected disk will be erased, so make sure you&apos;re selecting the correct disk.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../pages/diskpage.ui" line="126"/>
+        <source>Advanced...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../pages/diskpage.ui" line="136"/>
+        <source>Next</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../pages/diskpage.cpp" line="42"/>
+        <source>Select a location to install %1 to.</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>EraseConfirmPopover</name>
+    <message>
+        <location filename="../popovers/eraseconfirmpopover.ui" line="14"/>
+        <source>Form</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../popovers/eraseconfirmpopover.ui" line="51"/>
+        <source>THIS IS IT</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../popovers/eraseconfirmpopover.ui" line="58"/>
+        <source>There&apos;s no going back after this. Once the selected disk is erased, all the data on it is gone forever.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../popovers/eraseconfirmpopover.ui" line="68"/>
+        <source>Erase and Install</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../popovers/eraseconfirmpopover.cpp" line="30"/>
+        <source>Erase Disk and Install %1</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>FinishedPage</name>
+    <message>
+        <location filename="../pages/finishedpage.ui" line="14"/>
+        <source>Form</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../pages/finishedpage.ui" line="57"/>
+        <source>Installation Completed Successfully</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../pages/finishedpage.ui" line="64"/>
+        <source>The next time you start this device, you&apos;ll be asked to create a new user and set up some system settings.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../pages/finishedpage.ui" line="121"/>
+        <source>Power Off</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../pages/finishedpage.ui" line="132"/>
+        <location filename="../pages/finishedpage.ui" line="262"/>
+        <source>Reboot</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../pages/finishedpage.ui" line="164"/>
+        <source>Installation Failed</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../pages/finishedpage.ui" line="204"/>
+        <source>Your device may not be able to start if you reboot.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../pages/finishedpage.ui" line="214"/>
+        <source>From here, you&apos;ve got a few options:
+
+- You can retry the installation
+- You can exit and use recovery utilities to attempt to recover
+
+You can open a debug log with CTRL+L if you need to find out what went wrong.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../pages/finishedpage.ui" line="286"/>
+        <source>Exit</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../pages/finishedpage.ui" line="296"/>
+        <source>Start Over</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../pages/finishedpage.cpp" line="31"/>
+        <source>Sorry, we couldn&apos;t install %1.</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>InstallIpcManager</name>
+    <message>
+        <location filename="../installipcmanager.cpp" line="41"/>
+        <source>Starting Installation...</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>InstallManager</name>
+    <message>
+        <location filename="../install/installmanager.cpp" line="98"/>
+        <source>Installation complete!</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../install/installmanager.cpp" line="103"/>
+        <source>Installation failed!</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../install/installmanager.cpp" line="119"/>
+        <source>Finishing up...</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>InstallerData</name>
+    <message>
+        <location filename="../installerdata.cpp" line="61"/>
+        <location filename="../installerdata.cpp" line="63"/>
+        <source>Unknown</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>IssuesPage</name>
+    <message>
+        <location filename="../pages/issuespage.ui" line="14"/>
+        <source>Form</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../pages/issuespage.ui" line="35"/>
+        <source>Issues</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../pages/issuespage.ui" line="131"/>
+        <source>You&apos;re ready to continue!</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../pages/issuespage.cpp" line="45"/>
+        <source>Disk Space</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../pages/issuespage.cpp" line="46"/>
+        <source>This device doesn&apos;t have enough disk space to install %1.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../pages/issuespage.cpp" line="51"/>
+        <source>Memory</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../pages/issuespage.cpp" line="52"/>
+        <source>This system does not meet the minimum memory requirement of %1. Performance on the installed system may suffer as a result.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../pages/issuespage.cpp" line="57"/>
+        <source>Power</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../pages/issuespage.cpp" line="58"/>
+        <source>It is highly recommended that you connect this device to power before you start installing.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../pages/issuespage.cpp" line="63"/>
+        <source>Virtual Machine</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../pages/issuespage.cpp" line="64"/>
+        <source>Looks like you&apos;re installing %1 on a virtual machine. Performance on the installed system may suffer as a result.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../pages/issuespage.cpp" line="109"/>
+        <source>Ignore and Continue</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../pages/issuespage.cpp" line="109"/>
+        <source>Next</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../pages/issuespage.cpp" line="112"/>
+        <source>We&apos;re unable to install %1 on this device right now. Solve the issues below and then give it another go.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../pages/issuespage.cpp" line="114"/>
+        <source>The following issues may impact the installation. You should solve them before we continue.</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>MainWidget</name>
+    <message>
+        <location filename="../mainwidget.ui" line="14"/>
+        <source>Form</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="../mainwindow.cpp" line="34"/>
+        <source>Install %1</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="53"/>
+        <source>Exit?</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="53"/>
+        <source>%1 is not completely installed. Do you still want to exit?</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="59"/>
+        <source>Installation is ongoing</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="59"/>
+        <source>Sorry, installation cannot be cancelled at this stage.</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>ProgressPage</name>
+    <message>
+        <location filename="../pages/progresspage.ui" line="14"/>
+        <source>Form</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../pages/progresspage.ui" line="35"/>
+        <source>Installing</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../pages/progresspage.ui" line="45"/>
+        <source>Sit back and relax. We&apos;ll let you know when we&apos;re done.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../pages/progresspage.cpp" line="31"/>
+        <source>Preparing for installation</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>ReadyPage</name>
+    <message>
+        <location filename="../pages/readypage.ui" line="14"/>
+        <source>Form</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../pages/readypage.ui" line="35"/>
+        <source>Preparing for lift-off...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../pages/readypage.ui" line="42"/>
+        <source>That&apos;s everything we need right now.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../pages/readypage.cpp" line="40"/>
+        <source>Install %1</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>UnsquashState</name>
+    <message>
+        <location filename="../install/unsquashstate.cpp" line="33"/>
+        <location filename="../install/unsquashstate.cpp" line="44"/>
+        <source>Unsquashing Filesystem</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../install/unsquashstate.cpp" line="52"/>
+        <source>Unsquash Complete!</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>WelcomePage</name>
+    <message>
+        <location filename="../pages/welcomepage.ui" line="14"/>
+        <source>Form</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../pages/welcomepage.ui" line="48"/>
+        <source>Let&apos;s get going!</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../pages/welcomepage.ui" line="57"/>
+        <source>Language</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../pages/welcomepage.ui" line="83"/>
+        <source>Emergency</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../pages/welcomepage.ui" line="123"/>
+        <source>Let&apos;s go!</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../pages/welcomepage.cpp" line="31"/>
+        <source>Welcome to %1!</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
