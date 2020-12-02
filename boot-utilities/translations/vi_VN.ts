b97bb6e674056db22d866e770deecdba585609dd
Victor Tran <vicr12345@gmail.com>
2020-12-02 11:00:43 +0000

    Translated using Weblate (Vietnamese)
    Currently translated at 100.0% (16 of 16 strings)
    
    Translation: Scallop/Scallop Boot Utilities
    Translate-URL: https://translate.vicr123.com/projects/scallop/scallop-boot-utilities/vi/

--- boot-utilities/translations/vi_VN.ts
+++ boot-utilities/translations/vi_VN.ts
@@ -1,12 +1,12 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.1" sourcelanguage="en_US">
+<TS version="2.1" sourcelanguage="en_US" language="vi_VN">
 <context>
     <name>LanguagePopover</name>
     <message>
         <location filename="../languagepopover.ui" line="57"/>
         <source>Select a locale</source>
-        <translation type="unfinished"></translation>
+        <translation>Chọn một ngôn ngữ</translation>
     </message>
 </context>
 <context>
@@ -14,48 +14,48 @@
     <message>
         <location filename="../mainwindow.ui" line="122"/>
         <source>Language</source>
-        <translation type="unfinished"></translation>
+        <translation>Ngôn ngữ</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="148"/>
         <source>Emergency</source>
-        <translation type="unfinished"></translation>
+        <translation>Khẩn cấp</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="180"/>
         <source>Manage Disks</source>
-        <translation type="unfinished"></translation>
+        <translation>Quản lý đĩa</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="187"/>
         <source>View Files</source>
-        <translation type="unfinished"></translation>
+        <translation>Xem tệp</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="194"/>
         <source>Open Terminal</source>
-        <translation type="unfinished"></translation>
+        <translation>Mở thiết bị cuối</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="103"/>
         <source>Available installation and recovery tools are shown below.</source>
-        <translation type="unfinished"></translation>
+        <translation>Các tiện ích cho cài đặt và khôi phục sẽ hiện lên dưới đây.</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="173"/>
         <location filename="../mainwindow.cpp" line="91"/>
         <source>Install %1</source>
-        <translation type="unfinished"></translation>
+        <translation>Cài đặt %1</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="89"/>
         <source>Unknown</source>
-        <translation type="unfinished"></translation>
+        <translation>Không xác định</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="90"/>
         <source>Welcome to %1!</source>
-        <translation type="unfinished"></translation>
+        <translation>Chào mừng bạn với %1!</translation>
     </message>
 </context>
 <context>
@@ -63,32 +63,32 @@
     <message>
         <location filename="../powerpopover.ui" line="35"/>
         <source>Power Options</source>
-        <translation type="unfinished"></translation>
+        <translation>Tùy chọn nguồn</translation>
     </message>
     <message>
         <location filename="../powerpopover.ui" line="42"/>
         <source>What do you want to do now?</source>
-        <translation type="unfinished"></translation>
+        <translation>Bạn muốn làm gì bây giờ?</translation>
     </message>
     <message>
         <location filename="../powerpopover.ui" line="52"/>
         <source>Power Off</source>
-        <translation type="unfinished"></translation>
+        <translation>Tắt nguồn</translation>
     </message>
     <message>
         <location filename="../powerpopover.ui" line="59"/>
         <source>Turns off your computer.</source>
-        <translation type="unfinished"></translation>
+        <translation>Tắt máy tính của bạn.</translation>
     </message>
     <message>
         <location filename="../powerpopover.ui" line="69"/>
         <source>Reboot</source>
-        <translation type="unfinished"></translation>
+        <translation>Khởi động lại</translation>
     </message>
     <message>
         <location filename="../powerpopover.ui" line="76"/>
         <source>Turns off your computer and turns it on again</source>
-        <translation type="unfinished"></translation>
+        <translation>Tắt máy tính của bạn, rồi bật nó lại</translation>
     </message>
 </context>
 </TS>
