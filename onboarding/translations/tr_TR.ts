f2c2349162aedde49919a22ec243acb7c0157768
Atahan Reis <atahan.r@outlook.com>
2020-12-03 00:55:45 +0000

    Translated using Weblate (Turkish)
    Currently translated at 100.0% (21 of 21 strings)
    
    Translation: Scallop/Scallop Onboarding
    Translate-URL: https://translate.vicr123.com/projects/scallop/scallop-onboarding/tr/

--- onboarding/translations/tr_TR.ts
+++ onboarding/translations/tr_TR.ts
@@ -6,47 +6,47 @@
     <message>
         <location filename="../endsession.ui" line="35"/>
         <source>End Session</source>
-        <translation type="unfinished"></translation>
+        <translation>Oturum sonu</translation>
     </message>
     <message>
         <location filename="../endsession.ui" line="76"/>
         <source>What would you like to do now?</source>
-        <translation type="unfinished"></translation>
+        <translation>Şimdi ne yapmak istersin?</translation>
     </message>
     <message>
         <location filename="../endsession.ui" line="88"/>
         <source>Power Off</source>
-        <translation type="unfinished"></translation>
+        <translation>Kapat</translation>
     </message>
     <message>
         <location filename="../endsession.ui" line="95"/>
         <source>Turns off your computer.</source>
-        <translation type="unfinished"></translation>
+        <translation>Bilgisayarınızı kapatır.</translation>
     </message>
     <message>
         <location filename="../endsession.ui" line="105"/>
         <source>Reboot</source>
-        <translation type="unfinished"></translation>
+        <translation>Yeniden başlat</translation>
     </message>
     <message>
         <location filename="../endsession.ui" line="112"/>
         <source>Turns off your computer and turns it on again</source>
-        <translation type="unfinished"></translation>
+        <translation>Bilgisayarınızı kapatır ve tekrar açar</translation>
     </message>
     <message>
         <location filename="../endsession.ui" line="119"/>
         <source>Suspend</source>
-        <translation type="unfinished"></translation>
+        <translation>Askıya al</translation>
     </message>
     <message>
         <location filename="../endsession.ui" line="126"/>
         <source>Puts your computer in a low power state so it reopens quickly</source>
-        <translation type="unfinished"></translation>
+        <translation>Bilgisayarınızı düşük güç durumuna geçirir, böylece hızlı bir şekilde yeniden açılır</translation>
     </message>
     <message>
         <location filename="../endsession.ui" line="133"/>
         <source>Exit</source>
-        <translation type="unfinished"></translation>
+        <translation>Vazgeç</translation>
     </message>
 </context>
 <context>
@@ -54,34 +54,34 @@
     <message>
         <location filename="../OnboardingPages/onboardingcompleteoobe.ui" line="38"/>
         <source>That&apos;s everything!</source>
-        <translation type="unfinished"></translation>
+        <translation>Biz bittik!</translation>
     </message>
     <message>
         <location filename="../OnboardingPages/onboardingcompleteoobe.ui" line="61"/>
         <source>We hope you enjoy using theDesk!</source>
-        <translation type="unfinished"></translation>
+        <translation>Biz theDesk kullanarak beğeneceğinizi umuyoruz!</translation>
     </message>
     <message>
         <location filename="../OnboardingPages/onboardingcompleteoobe.ui" line="86"/>
         <source>Get Started</source>
-        <translation type="unfinished"></translation>
+        <translation>Başlamak</translation>
     </message>
     <message>
         <location filename="../OnboardingPages/onboardingcompleteoobe.cpp" line="58"/>
         <location filename="../OnboardingPages/onboardingcompleteoobe.cpp" line="64"/>
         <source>We hope you enjoy using %1!</source>
-        <translation type="unfinished"></translation>
+        <translation>%1&apos;i kullanmaktan keyif alacağınızı umuyoruz!</translation>
     </message>
     <message>
         <location filename="../OnboardingPages/onboardingcompleteoobe.cpp" line="58"/>
         <location filename="../OnboardingPages/onboardingcompleteoobe.cpp" line="64"/>
         <source>your new device</source>
-        <translation type="unfinished"></translation>
+        <translation>yeni cihazın</translation>
     </message>
     <message>
         <location filename="../OnboardingPages/onboardingcompleteoobe.cpp" line="77"/>
         <source>Done</source>
-        <translation type="unfinished"></translation>
+        <translation>Bitti</translation>
     </message>
 </context>
 <context>
@@ -90,32 +90,32 @@
         <location filename="../OnboardingPages/onboardinghostname.ui" line="35"/>
         <location filename="../OnboardingPages/onboardinghostname.cpp" line="98"/>
         <source>Device Name</source>
-        <translation type="unfinished"></translation>
+        <translation>Cihaz adı</translation>
     </message>
     <message>
         <location filename="../OnboardingPages/onboardinghostname.ui" line="59"/>
         <source>Family PC</source>
-        <translation type="unfinished"></translation>
+        <translation>Aile bilgisayarı</translation>
     </message>
     <message>
         <location filename="../OnboardingPages/onboardinghostname.ui" line="66"/>
         <source>New Device Name</source>
-        <translation type="unfinished"></translation>
+        <translation>Yeni Cihaz Adı</translation>
     </message>
     <message>
         <location filename="../OnboardingPages/onboardinghostname.ui" line="73"/>
         <source>Set a device name for this device. It&apos;s used to identify this device over the network, and will also be used as the device name over Bluetooth.</source>
-        <translation type="unfinished"></translation>
+        <translation>Bu cihaz için bir cihaz adı belirleyin. Bu cihazı ağ üzerinden tanımlamak için kullanılır ve ayrıca Bluetooth üzerinden cihaz adı olarak da kullanılacaktır.</translation>
     </message>
     <message>
         <location filename="../OnboardingPages/onboardinghostname.ui" line="83"/>
         <source>Network Compatible Name</source>
-        <translation type="unfinished"></translation>
+        <translation>Ağ Uyumlu Ad</translation>
     </message>
     <message>
         <location filename="../OnboardingPages/onboardinghostname.ui" line="158"/>
         <source>Next</source>
-        <translation type="unfinished"></translation>
+        <translation>Sonraki</translation>
     </message>
 </context>
 </TS>
