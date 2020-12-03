37f674fce0bed6fe68a50e61dea61a524d720596
Victor Tran <vicr12345@gmail.com>
2020-12-03 01:02:53 +0000

    Translated using Weblate (Vietnamese)
    Currently translated at 100.0% (21 of 21 strings)
    
    Translation: Scallop/Scallop Onboarding
    Translate-URL: https://translate.vicr123.com/projects/scallop/scallop-onboarding/vi/

--- onboarding/translations/vi_VN.ts
+++ onboarding/translations/vi_VN.ts
@@ -1,52 +1,52 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.1" sourcelanguage="en_US">
+<TS version="2.1" sourcelanguage="en_US" language="vi_VN">
 <context>
     <name>EndSession</name>
     <message>
         <location filename="../endsession.ui" line="35"/>
         <source>End Session</source>
-        <translation type="unfinished"></translation>
+        <translation>Kết phiên</translation>
     </message>
     <message>
         <location filename="../endsession.ui" line="76"/>
         <source>What would you like to do now?</source>
-        <translation type="unfinished"></translation>
+        <translation>Bạn muốn làm gì bây giờ?</translation>
     </message>
     <message>
         <location filename="../endsession.ui" line="88"/>
         <source>Power Off</source>
-        <translation type="unfinished"></translation>
+        <translation>Tắt nguồn</translation>
     </message>
     <message>
         <location filename="../endsession.ui" line="95"/>
         <source>Turns off your computer.</source>
-        <translation type="unfinished"></translation>
+        <translation>Tắt máy tính của bạn.</translation>
     </message>
     <message>
         <location filename="../endsession.ui" line="105"/>
         <source>Reboot</source>
-        <translation type="unfinished"></translation>
+        <translation>Khởi động lại</translation>
     </message>
     <message>
         <location filename="../endsession.ui" line="112"/>
         <source>Turns off your computer and turns it on again</source>
-        <translation type="unfinished"></translation>
+        <translation>Tắt máy tính của bạn, rồi bật nó lại</translation>
     </message>
     <message>
         <location filename="../endsession.ui" line="119"/>
         <source>Suspend</source>
-        <translation type="unfinished"></translation>
+        <translation>Tạm dừng</translation>
     </message>
     <message>
         <location filename="../endsession.ui" line="126"/>
         <source>Puts your computer in a low power state so it reopens quickly</source>
-        <translation type="unfinished"></translation>
+        <translation>Đặt máy tính của bạn vào chế độ tiêu ít năng lượng để cho nó mở lại nhanh</translation>
     </message>
     <message>
         <location filename="../endsession.ui" line="133"/>
         <source>Exit</source>
-        <translation type="unfinished"></translation>
+        <translation>Thoát</translation>
     </message>
 </context>
 <context>
@@ -54,34 +54,34 @@
     <message>
         <location filename="../OnboardingPages/onboardingcompleteoobe.ui" line="38"/>
         <source>That&apos;s everything!</source>
-        <translation type="unfinished"></translation>
+        <translation>Đó là tất cả!</translation>
     </message>
     <message>
         <location filename="../OnboardingPages/onboardingcompleteoobe.ui" line="61"/>
         <source>We hope you enjoy using theDesk!</source>
-        <translation type="unfinished"></translation>
+        <translation>Chúng tôi hy vọng bạn thích sử dụng theDesk!</translation>
     </message>
     <message>
         <location filename="../OnboardingPages/onboardingcompleteoobe.ui" line="86"/>
         <source>Get Started</source>
-        <translation type="unfinished"></translation>
+        <translation>Bắt đầu</translation>
     </message>
     <message>
         <location filename="../OnboardingPages/onboardingcompleteoobe.cpp" line="58"/>
         <location filename="../OnboardingPages/onboardingcompleteoobe.cpp" line="64"/>
         <source>We hope you enjoy using %1!</source>
-        <translation type="unfinished"></translation>
+        <translation>Chúng tôi hy vọng bạn thích sử dụng %1!</translation>
     </message>
     <message>
         <location filename="../OnboardingPages/onboardingcompleteoobe.cpp" line="58"/>
         <location filename="../OnboardingPages/onboardingcompleteoobe.cpp" line="64"/>
         <source>your new device</source>
-        <translation type="unfinished"></translation>
+        <translation>thiết bị mới của bạn</translation>
     </message>
     <message>
         <location filename="../OnboardingPages/onboardingcompleteoobe.cpp" line="77"/>
         <source>Done</source>
-        <translation type="unfinished"></translation>
+        <translation>Hoàn thành</translation>
     </message>
 </context>
 <context>
@@ -90,32 +90,32 @@
         <location filename="../OnboardingPages/onboardinghostname.ui" line="35"/>
         <location filename="../OnboardingPages/onboardinghostname.cpp" line="98"/>
         <source>Device Name</source>
-        <translation type="unfinished"></translation>
+        <translation>Tên thiết bị</translation>
     </message>
     <message>
         <location filename="../OnboardingPages/onboardinghostname.ui" line="59"/>
         <source>Family PC</source>
-        <translation type="unfinished"></translation>
+        <translation>PC gia đình</translation>
     </message>
     <message>
         <location filename="../OnboardingPages/onboardinghostname.ui" line="66"/>
         <source>New Device Name</source>
-        <translation type="unfinished"></translation>
+        <translation>Tên thiết bị mới</translation>
     </message>
     <message>
         <location filename="../OnboardingPages/onboardinghostname.ui" line="73"/>
         <source>Set a device name for this device. It&apos;s used to identify this device over the network, and will also be used as the device name over Bluetooth.</source>
-        <translation type="unfinished"></translation>
+        <translation>Chọn một tên cho thiết bị này. Nó sẽ bị dùng để nhận định thiết bị này qua mạng, và cũng sử dụng cho tên thiết bị này qua Bluetooth.</translation>
     </message>
     <message>
         <location filename="../OnboardingPages/onboardinghostname.ui" line="83"/>
         <source>Network Compatible Name</source>
-        <translation type="unfinished"></translation>
+        <translation>Tên mạng tương thích</translation>
     </message>
     <message>
         <location filename="../OnboardingPages/onboardinghostname.ui" line="158"/>
         <source>Next</source>
-        <translation type="unfinished"></translation>
+        <translation>Tiếp</translation>
     </message>
 </context>
 </TS>
