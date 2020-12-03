743ffdc3281aef98673385aba94924bde2f081bc
Atahan Reis <atahan.r@outlook.com>
2020-12-03 00:14:31 +0000

    Translated using Weblate (Turkish)
    Currently translated at 100.0% (16 of 16 strings)
    
    Translation: Scallop/Scallop Boot Utilities
    Translate-URL: https://translate.vicr123.com/projects/scallop/scallop-boot-utilities/tr/

--- boot-utilities/translations/tr_TR.ts
+++ boot-utilities/translations/tr_TR.ts
@@ -6,7 +6,7 @@
     <message>
         <location filename="../languagepopover.ui" line="57"/>
         <source>Select a locale</source>
-        <translation type="unfinished"></translation>
+        <translation>Bir yerel ayar seçin</translation>
     </message>
 </context>
 <context>
@@ -14,48 +14,48 @@
     <message>
         <location filename="../mainwindow.ui" line="122"/>
         <source>Language</source>
-        <translation type="unfinished"></translation>
+        <translation>Dil</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="148"/>
         <source>Emergency</source>
-        <translation type="unfinished"></translation>
+        <translation>Alil Durum</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="180"/>
         <source>Manage Disks</source>
-        <translation type="unfinished"></translation>
+        <translation>Diskleri Yönet</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="187"/>
         <source>View Files</source>
-        <translation type="unfinished"></translation>
+        <translation>Dosyaları görüntüle</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="194"/>
         <source>Open Terminal</source>
-        <translation type="unfinished"></translation>
+        <translation>Terminali açın</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="103"/>
         <source>Available installation and recovery tools are shown below.</source>
-        <translation type="unfinished"></translation>
+        <translation>Mevcut kurulum ve kurtarma araçları aşağıda gösterilmektedir.</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="173"/>
         <location filename="../mainwindow.cpp" line="91"/>
         <source>Install %1</source>
-        <translation type="unfinished"></translation>
+        <translation>%1 yükleyin</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="89"/>
         <source>Unknown</source>
-        <translation type="unfinished"></translation>
+        <translation>Bilinmeyen</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="90"/>
         <source>Welcome to %1!</source>
-        <translation type="unfinished"></translation>
+        <translation>%1&apos;e hoş geldin!</translation>
     </message>
 </context>
 <context>
@@ -63,32 +63,32 @@
     <message>
         <location filename="../powerpopover.ui" line="35"/>
         <source>Power Options</source>
-        <translation type="unfinished"></translation>
+        <translation>Güç seçenekleri</translation>
     </message>
     <message>
         <location filename="../powerpopover.ui" line="42"/>
         <source>What do you want to do now?</source>
-        <translation type="unfinished"></translation>
+        <translation>Şimdi ne yapmak istiyorsun?</translation>
     </message>
     <message>
         <location filename="../powerpopover.ui" line="52"/>
         <source>Power Off</source>
-        <translation type="unfinished"></translation>
+        <translation>Kapat</translation>
     </message>
     <message>
         <location filename="../powerpopover.ui" line="59"/>
         <source>Turns off your computer.</source>
-        <translation type="unfinished"></translation>
+        <translation>Bilgisayarınızı kapatır.</translation>
     </message>
     <message>
         <location filename="../powerpopover.ui" line="69"/>
         <source>Reboot</source>
-        <translation type="unfinished"></translation>
+        <translation>Yeniden başlat</translation>
     </message>
     <message>
         <location filename="../powerpopover.ui" line="76"/>
         <source>Turns off your computer and turns it on again</source>
-        <translation type="unfinished"></translation>
+        <translation>Bilgisayarınızı kapatır ve tekrar açar</translation>
     </message>
 </context>
 </TS>
