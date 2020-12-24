2e5899e6d461ef329f94bc1975208781f78f9b85
Atahan Reis <atahanreis@icloud.com>
2020-12-24 19:12:00 +0000

    Translated using Weblate (English (United Kingdom))
    Currently translated at 100.0% (16 of 16 strings)
    
    Translation: Scallop/Scallop Boot Utilities
    Translate-URL: https://translate.vicr123.com/projects/scallop/scallop-boot-utilities/en_GB/

--- boot-utilities/translations/en_GB.ts
+++ boot-utilities/translations/en_GB.ts
@@ -1,12 +1,12 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.1" sourcelanguage="en_US">
+<TS version="2.1" sourcelanguage="en_US" language="en_GB">
 <context>
     <name>LanguagePopover</name>
     <message>
         <location filename="../languagepopover.ui" line="57"/>
         <source>Select a locale</source>
-        <translation type="unfinished"></translation>
+        <translation>Select a locale</translation>
     </message>
 </context>
 <context>
@@ -14,47 +14,47 @@
     <message>
         <location filename="../mainwindow.ui" line="122"/>
         <source>Language</source>
-        <translation type="unfinished"></translation>
+        <translation>Language</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="148"/>
         <source>Emergency</source>
-        <translation type="unfinished"></translation>
+        <translation>Emergency</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="180"/>
         <source>Manage Disks</source>
-        <translation type="unfinished"></translation>
+        <translation>Manage Disks</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="187"/>
         <source>View Files</source>
-        <translation type="unfinished"></translation>
+        <translation>View Files</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="194"/>
         <source>Open Terminal</source>
-        <translation type="unfinished"></translation>
+        <translation>Open Terminal</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="103"/>
         <source>Available installation and recovery tools are shown below.</source>
-        <translation type="unfinished"></translation>
+        <translation>Available installation and recovery tools are shown below.</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="163"/>
         <source>Install %1</source>
-        <translation type="unfinished"></translation>
+        <translation>Install %1</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="161"/>
         <source>Unknown</source>
-        <translation type="unfinished"></translation>
+        <translation>Unknown</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="162"/>
         <source>Welcome to %1!</source>
-        <translation type="unfinished"></translation>
+        <translation>Welcome to %1!</translation>
     </message>
 </context>
 <context>
@@ -62,32 +62,32 @@
     <message>
         <location filename="../powerpopover.ui" line="35"/>
         <source>Power Options</source>
-        <translation type="unfinished"></translation>
+        <translation>Power Options</translation>
     </message>
     <message>
         <location filename="../powerpopover.ui" line="42"/>
         <source>What do you want to do now?</source>
-        <translation type="unfinished"></translation>
+        <translation>What do you want to do now?</translation>
     </message>
     <message>
         <location filename="../powerpopover.ui" line="52"/>
         <source>Power Off</source>
-        <translation type="unfinished"></translation>
+        <translation>Power Off</translation>
     </message>
     <message>
         <location filename="../powerpopover.ui" line="59"/>
         <source>Turns off your computer.</source>
-        <translation type="unfinished"></translation>
+        <translation>Turns off your computer.</translation>
     </message>
     <message>
         <location filename="../powerpopover.ui" line="69"/>
         <source>Reboot</source>
-        <translation type="unfinished"></translation>
+        <translation>Reboot</translation>
     </message>
     <message>
         <location filename="../powerpopover.ui" line="76"/>
         <source>Turns off your computer and turns it on again</source>
-        <translation type="unfinished"></translation>
+        <translation>Turns off your computer and turns it on again</translation>
     </message>
 </context>
 </TS>
