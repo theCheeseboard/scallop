1709af2776dd91be46a18bd6643f11da62b9a98f
Atahan Reis <atahanreis@icloud.com>
2020-12-24 19:14:00 +0000

    Translated using Weblate (English (United Kingdom))
    Currently translated at 100.0% (21 of 21 strings)
    
    Translation: Scallop/Scallop Onboarding
    Translate-URL: https://translate.vicr123.com/projects/scallop/scallop-onboarding/en_GB/

--- onboarding/translations/en_GB.ts
+++ onboarding/translations/en_GB.ts
@@ -1,52 +1,52 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.1" sourcelanguage="en_US">
+<TS version="2.1" sourcelanguage="en_US" language="en_GB">
 <context>
     <name>EndSession</name>
     <message>
         <location filename="../endsession.ui" line="35"/>
         <source>End Session</source>
-        <translation type="unfinished"></translation>
+        <translation>End Session</translation>
     </message>
     <message>
         <location filename="../endsession.ui" line="76"/>
         <source>What would you like to do now?</source>
-        <translation type="unfinished"></translation>
+        <translation>What would you like to do now?</translation>
     </message>
     <message>
         <location filename="../endsession.ui" line="88"/>
         <source>Power Off</source>
-        <translation type="unfinished"></translation>
+        <translation>Power Off</translation>
     </message>
     <message>
         <location filename="../endsession.ui" line="95"/>
         <source>Turns off your computer.</source>
-        <translation type="unfinished"></translation>
+        <translation>Turns off your computer.</translation>
     </message>
     <message>
         <location filename="../endsession.ui" line="105"/>
         <source>Reboot</source>
-        <translation type="unfinished"></translation>
+        <translation>Reboot</translation>
     </message>
     <message>
         <location filename="../endsession.ui" line="112"/>
         <source>Turns off your computer and turns it on again</source>
-        <translation type="unfinished"></translation>
+        <translation>Turns off your computer and turns it on again</translation>
     </message>
     <message>
         <location filename="../endsession.ui" line="119"/>
         <source>Suspend</source>
-        <translation type="unfinished"></translation>
+        <translation>Suspend</translation>
     </message>
     <message>
         <location filename="../endsession.ui" line="126"/>
         <source>Puts your computer in a low power state so it reopens quickly</source>
-        <translation type="unfinished"></translation>
+        <translation>Puts your computer in a low power state so it reopens quickly</translation>
     </message>
     <message>
         <location filename="../endsession.ui" line="133"/>
         <source>Exit</source>
-        <translation type="unfinished"></translation>
+        <translation>Exit</translation>
     </message>
 </context>
 <context>
@@ -54,34 +54,34 @@
     <message>
         <location filename="../OnboardingPages/onboardingcompleteoobe.ui" line="38"/>
         <source>That&apos;s everything!</source>
-        <translation type="unfinished"></translation>
+        <translation>That&apos;s everything!</translation>
     </message>
     <message>
         <location filename="../OnboardingPages/onboardingcompleteoobe.ui" line="61"/>
         <source>We hope you enjoy using theDesk!</source>
-        <translation type="unfinished"></translation>
+        <translation>We hope you enjoy using theDesk!</translation>
     </message>
     <message>
         <location filename="../OnboardingPages/onboardingcompleteoobe.ui" line="86"/>
         <source>Get Started</source>
-        <translation type="unfinished"></translation>
+        <translation>Get Started</translation>
     </message>
     <message>
         <location filename="../OnboardingPages/onboardingcompleteoobe.cpp" line="58"/>
         <location filename="../OnboardingPages/onboardingcompleteoobe.cpp" line="64"/>
         <source>We hope you enjoy using %1!</source>
-        <translation type="unfinished"></translation>
+        <translation>We hope you enjoy using %1!</translation>
     </message>
     <message>
         <location filename="../OnboardingPages/onboardingcompleteoobe.cpp" line="58"/>
         <location filename="../OnboardingPages/onboardingcompleteoobe.cpp" line="64"/>
         <source>your new device</source>
-        <translation type="unfinished"></translation>
+        <translation>your new device</translation>
     </message>
     <message>
         <location filename="../OnboardingPages/onboardingcompleteoobe.cpp" line="77"/>
         <source>Done</source>
-        <translation type="unfinished"></translation>
+        <translation>Done</translation>
     </message>
 </context>
 <context>
@@ -90,32 +90,32 @@
         <location filename="../OnboardingPages/onboardinghostname.ui" line="35"/>
         <location filename="../OnboardingPages/onboardinghostname.cpp" line="98"/>
         <source>Device Name</source>
-        <translation type="unfinished"></translation>
+        <translation>Device Name</translation>
     </message>
     <message>
         <location filename="../OnboardingPages/onboardinghostname.ui" line="59"/>
         <source>Family PC</source>
-        <translation type="unfinished"></translation>
+        <translation>Family PC</translation>
     </message>
     <message>
         <location filename="../OnboardingPages/onboardinghostname.ui" line="66"/>
         <source>New Device Name</source>
-        <translation type="unfinished"></translation>
+        <translation>New Device Name</translation>
     </message>
     <message>
         <location filename="../OnboardingPages/onboardinghostname.ui" line="73"/>
         <source>Set a device name for this device. It&apos;s used to identify this device over the network, and will also be used as the device name over Bluetooth.</source>
-        <translation type="unfinished"></translation>
+        <translation>Set a device name for this device. It&apos;s used to identify this device over the network, and will also be used as the device name over Bluetooth.</translation>
     </message>
     <message>
         <location filename="../OnboardingPages/onboardinghostname.ui" line="83"/>
         <source>Network Compatible Name</source>
-        <translation type="unfinished"></translation>
+        <translation>Network Compatible Name</translation>
     </message>
     <message>
         <location filename="../OnboardingPages/onboardinghostname.ui" line="158"/>
         <source>Next</source>
-        <translation type="unfinished"></translation>
+        <translation>Next</translation>
     </message>
 </context>
 </TS>
