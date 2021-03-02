6606dd9d130c64543c7bf2e62d094eccde261310
S8msGITcode <sammaslow8@gmail.com>
2021-03-02 22:17:09 +0000

    Translated using Weblate (Dutch)
    Currently translated at 95.2% (20 of 21 strings)
    
    Translation: Scallop/Scallop Onboarding
    Translate-URL: https://translate.vicr123.com/projects/scallop/scallop-onboarding/nl/

--- onboarding/translations/nl_NL.ts
+++ onboarding/translations/nl_NL.ts
@@ -6,32 +6,32 @@
     <message>
         <location filename="../endsession.ui" line="35"/>
         <source>End Session</source>
-        <translation type="unfinished"></translation>
+        <translation>Beëindig Sessie</translation>
     </message>
     <message>
         <location filename="../endsession.ui" line="76"/>
         <source>What would you like to do now?</source>
-        <translation type="unfinished"></translation>
+        <translation>Wat wil je nu doen?</translation>
     </message>
     <message>
         <location filename="../endsession.ui" line="88"/>
         <source>Power Off</source>
-        <translation type="unfinished"></translation>
+        <translation>UItschakelen</translation>
     </message>
     <message>
         <location filename="../endsession.ui" line="95"/>
         <source>Turns off your computer.</source>
-        <translation type="unfinished"></translation>
+        <translation>Zet uw computer uit.</translation>
     </message>
     <message>
         <location filename="../endsession.ui" line="105"/>
         <source>Reboot</source>
-        <translation type="unfinished"></translation>
+        <translation>Herstarten</translation>
     </message>
     <message>
         <location filename="../endsession.ui" line="112"/>
         <source>Turns off your computer and turns it on again</source>
-        <translation type="unfinished"></translation>
+        <translation>Zet uw computer uit en weer aan</translation>
     </message>
     <message>
         <location filename="../endsession.ui" line="119"/>
@@ -41,12 +41,12 @@
     <message>
         <location filename="../endsession.ui" line="126"/>
         <source>Puts your computer in a low power state so it reopens quickly</source>
-        <translation type="unfinished"></translation>
+        <translation>Zet de computer in een laag-energie stand zodat uw apparaat in een ogenblik opstart</translation>
     </message>
     <message>
         <location filename="../endsession.ui" line="133"/>
         <source>Exit</source>
-        <translation type="unfinished"></translation>
+        <translation>Sluiten</translation>
     </message>
 </context>
 <context>
@@ -54,34 +54,34 @@
     <message>
         <location filename="../OnboardingPages/onboardingcompleteoobe.ui" line="38"/>
         <source>That&apos;s everything!</source>
-        <translation type="unfinished"></translation>
+        <translation>Dat was alles!</translation>
     </message>
     <message>
         <location filename="../OnboardingPages/onboardingcompleteoobe.ui" line="61"/>
         <source>We hope you enjoy using theDesk!</source>
-        <translation type="unfinished"></translation>
+        <translation>We hopen dat u veel plezier heeft met theDesk!</translation>
     </message>
     <message>
         <location filename="../OnboardingPages/onboardingcompleteoobe.ui" line="86"/>
         <source>Get Started</source>
-        <translation type="unfinished"></translation>
+        <translation>Ga aan de slag</translation>
     </message>
     <message>
         <location filename="../OnboardingPages/onboardingcompleteoobe.cpp" line="58"/>
         <location filename="../OnboardingPages/onboardingcompleteoobe.cpp" line="64"/>
         <source>We hope you enjoy using %1!</source>
-        <translation type="unfinished"></translation>
+        <translation>We hopen dat u veel plezier heeft!</translation>
     </message>
     <message>
         <location filename="../OnboardingPages/onboardingcompleteoobe.cpp" line="58"/>
         <location filename="../OnboardingPages/onboardingcompleteoobe.cpp" line="64"/>
         <source>your new device</source>
-        <translation type="unfinished"></translation>
+        <translation>uw nieuwe apparaat</translation>
     </message>
     <message>
         <location filename="../OnboardingPages/onboardingcompleteoobe.cpp" line="77"/>
         <source>Done</source>
-        <translation type="unfinished"></translation>
+        <translation>Klaar</translation>
     </message>
 </context>
 <context>
@@ -90,32 +90,32 @@
         <location filename="../OnboardingPages/onboardinghostname.ui" line="35"/>
         <location filename="../OnboardingPages/onboardinghostname.cpp" line="98"/>
         <source>Device Name</source>
-        <translation type="unfinished"></translation>
+        <translation>Apparaatnaam</translation>
     </message>
     <message>
         <location filename="../OnboardingPages/onboardinghostname.ui" line="59"/>
         <source>Family PC</source>
-        <translation type="unfinished"></translation>
+        <translation>Famillie PC</translation>
     </message>
     <message>
         <location filename="../OnboardingPages/onboardinghostname.ui" line="66"/>
         <source>New Device Name</source>
-        <translation type="unfinished"></translation>
+        <translation>Nieuw apparaatnaam</translation>
     </message>
     <message>
         <location filename="../OnboardingPages/onboardinghostname.ui" line="73"/>
         <source>Set a device name for this device. It&apos;s used to identify this device over the network, and will also be used as the device name over Bluetooth.</source>
-        <translation type="unfinished"></translation>
+        <translation>Stel een apparaatnaam in voor dit apparaat. Het wordt gebruikt om dit apparaat te identificeren over het internet, dit geldt ook voor Bluetooth™.</translation>
     </message>
     <message>
         <location filename="../OnboardingPages/onboardinghostname.ui" line="83"/>
         <source>Network Compatible Name</source>
-        <translation type="unfinished"></translation>
+        <translation>Netwerk Verinigbare Naam</translation>
     </message>
     <message>
         <location filename="../OnboardingPages/onboardinghostname.ui" line="158"/>
         <source>Next</source>
-        <translation type="unfinished"></translation>
+        <translation>Volgende</translation>
     </message>
 </context>
 </TS>
