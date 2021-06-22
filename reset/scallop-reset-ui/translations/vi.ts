c631610e49ae9b951606c5220d6b85100d9fe45c
Victor Tran <vicr12345@gmail.com>
2021-06-22 10:46:17 +0000

    Translated using Weblate (Vietnamese)
    Currently translated at 100.0% (17 of 17 strings)
    
    Translation: Scallop/Scallop Reset
    Translate-URL: https://translate.vicr123.com/projects/scallop/scallop-reset/vi/

--- reset/scallop-reset-ui/translations/vi.ts
+++ reset/scallop-reset-ui/translations/vi.ts
@@ -6,27 +6,27 @@
     <message>
         <location filename="../downloadprogress.ui" line="58"/>
         <source>Preparing to Reset</source>
-        <translation type="unfinished"></translation>
+        <translation>Đang chuẩn bị đặt lại</translation>
     </message>
     <message>
         <location filename="../downloadprogress.ui" line="73"/>
         <source>We&apos;re preparing this device to be reset. Stay connected to the Internet. This will take a while.</source>
-        <translation type="unfinished"></translation>
+        <translation>Chúng tôi đang chuẩn bị thiết bị này để đặt lại. Vui lòng giữ kết nối với Internet. Điều này sẽ mất một thời gian.</translation>
     </message>
     <message>
         <location filename="../downloadprogress.cpp" line="60"/>
         <source>Reset Failed</source>
-        <translation type="unfinished"></translation>
+        <translation>Không thể đặt lại</translation>
     </message>
     <message>
         <location filename="../downloadprogress.cpp" line="60"/>
         <source>We weren&apos;t able to download the recovery image. Please try again later.</source>
-        <translation type="unfinished"></translation>
+        <translation>Chúng tôi không thể tải xuống ảnh khôi phục. Vui lòng thử lại sau.</translation>
     </message>
     <message>
         <location filename="../downloadprogress.cpp" line="67"/>
         <source>Downloaded %1 of %2</source>
-        <translation type="unfinished"></translation>
+        <translation>Đã tải xuống %1 trên %2</translation>
     </message>
 </context>
 <context>
@@ -34,22 +34,22 @@
     <message>
         <location filename="../finalresetpopover.ui" line="51"/>
         <source>THIS IS IT</source>
-        <translation type="unfinished"></translation>
+        <translation>SẴN SÀNG CHƯA?</translation>
     </message>
     <message>
         <location filename="../finalresetpopover.ui" line="58"/>
         <source>There&apos;s no going back after this. Once this device is reset, all your data is gone forever.</source>
-        <translation type="unfinished"></translation>
+        <translation>Bạn không thể quay lại sau điểm này. Sau khi thiết bị này bị đặt lại, các dữ liệu của bạn sẽ bị mất vĩnh viễn.</translation>
     </message>
     <message>
         <location filename="../finalresetpopover.ui" line="68"/>
         <source>Reboot and reset this device</source>
-        <translation type="unfinished"></translation>
+        <translation>Khởi động lại và đặt lại thiết bị này</translation>
     </message>
     <message>
         <location filename="../finalresetpopover.cpp" line="28"/>
         <source>Reset this device</source>
-        <translation type="unfinished"></translation>
+        <translation>Đặt lại thiết bị</translation>
     </message>
 </context>
 <context>
@@ -58,33 +58,33 @@
         <location filename="../mainwindow.ui" line="14"/>
         <location filename="../mainwindow.ui" line="55"/>
         <source>Reset this device</source>
-        <translation type="unfinished"></translation>
+        <translation>Đặt lại thiết bị</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="84"/>
         <source>Yes, reset this device</source>
-        <translation type="unfinished"></translation>
+        <translation>Có, đặt lại thiết bị này</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="87"/>
         <source>All data will be deleted and this device will restart.</source>
-        <translation type="unfinished"></translation>
+        <translation>Các dữ liệu sẽ bị xóa và thiết bị này sẽ khởi động lại.</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="94"/>
         <source>No, don&apos;t reset this device</source>
-        <translation type="unfinished"></translation>
+        <translation>Không, đừng đặt lại thiết bị này</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="97"/>
         <source>Your data will be preserved.</source>
-        <translation type="unfinished"></translation>
+        <translation>Dữ liệu của bạn sẽ bị giữ.</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="64"/>
         <location filename="../mainwindow.cpp" line="66"/>
         <source>Unknown</source>
-        <translation type="unfinished"></translation>
+        <translation>Không xác định</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="69"/>
@@ -99,7 +99,17 @@ Resetting this device will erase all personal data **for all accounts.** This in
 **READY TO DO THIS?**
 
 To get the ball rolling, we&apos;ll restart this device and start removing all the data on it. Once the reset is complete, a new copy of %1 will be installed. This process can take a while.</source>
-        <translation type="unfinished"></translation>
+        <translation>**CÀI DÂY AN TOÀN!**
+
+Hoạt động đặt lại thiết bị này sẽ xóa các dữ liệu cá nhân **cho mọi tài khoản.** Dữ liệu này bao gồm:
+- Tài liệu
+- Hình ảnh
+- Các ứng dụng đã cài đặt
+- Các cài đặt đã thay đổi
+
+**SẴN SÀNG LÀM NÀY CHƯA?**
+
+Để bắt đầu, chúng tôi sẽ khởi động lại thiết bị này và bắt đầu xóa các dữ liệu trên nó. Sau khi hoạt động đặt lại hoàn tất, một bản %1 mới sẽ bị cài đặt. Quá trình này sẽ mất một thời gian.</translation>
     </message>
 </context>
 <context>
@@ -107,7 +117,7 @@ To get the ball rolling, we&apos;ll restart this device and start removing all t
     <message>
         <location filename="../main.cpp" line="31"/>
         <source>System Configuration Tool</source>
-        <translation type="unfinished"></translation>
+        <translation>Công cụ cấu hình hệ thống</translation>
     </message>
 </context>
 </TS>
