6e7295f7655176c3a137f5d07cb854459e638a2d
Tobias SN <tobi897a2@outlook.dk>
2021-07-01 17:20:16 +0000

    Added translation using Weblate (Danish)

---
+++ reset/scallop-reset-ui/translations/da.ts
@@ -0,0 +1,113 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" sourcelanguage="en_US" language="da">
+<context>
+    <name>DownloadProgress</name>
+    <message>
+        <location filename="../downloadprogress.ui" line="58"/>
+        <source>Preparing to Reset</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../downloadprogress.ui" line="73"/>
+        <source>We&apos;re preparing this device to be reset. Stay connected to the Internet. This will take a while.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../downloadprogress.cpp" line="60"/>
+        <source>Reset Failed</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../downloadprogress.cpp" line="60"/>
+        <source>We weren&apos;t able to download the recovery image. Please try again later.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../downloadprogress.cpp" line="67"/>
+        <source>Downloaded %1 of %2</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>FinalResetPopover</name>
+    <message>
+        <location filename="../finalresetpopover.ui" line="51"/>
+        <source>THIS IS IT</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../finalresetpopover.ui" line="58"/>
+        <source>There&apos;s no going back after this. Once this device is reset, all your data is gone forever.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../finalresetpopover.ui" line="68"/>
+        <source>Reboot and reset this device</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../finalresetpopover.cpp" line="28"/>
+        <source>Reset this device</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="../mainwindow.ui" line="14"/>
+        <location filename="../mainwindow.ui" line="125"/>
+        <source>Reset this device</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="154"/>
+        <source>Yes, reset this device</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="157"/>
+        <source>All data will be deleted and this device will restart.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="164"/>
+        <source>No, don&apos;t reset this device</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="167"/>
+        <source>Your data will be preserved.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="97"/>
+        <location filename="../mainwindow.cpp" line="99"/>
+        <source>Unknown</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="102"/>
+        <source>**BUCKLE UP!**
+
+Resetting this device will erase all personal data **for all accounts.** This includes:
+- Documents
+- Pictures
+- Installed apps
+- Any changed settings
+
+**READY TO DO THIS?**
+
+To get the ball rolling, we&apos;ll restart this device and start removing all the data on it. Once the reset is complete, a new copy of %1 will be installed. This process can take a while.</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>main</name>
+    <message>
+        <location filename="../main.cpp" line="31"/>
+        <source>System Configuration Tool</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
