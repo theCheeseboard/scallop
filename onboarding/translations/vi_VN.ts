fcee85eca1e4a5696f58cd5ae478155d7627172e
Parlance <parlance@vicr123.com>
2022-02-10 06:00:10 +0000

    Update Translations

--- onboarding/translations/vi_VN.ts
+++ onboarding/translations/vi_VN.ts
@@ -1,121 +1,121 @@
-<?xml version="1.0" encoding="utf-8"?>
-<!DOCTYPE TS>
+<?xml version="1.0" encoding="utf-16"?>
+<!DOCTYPE TS []>
 <TS version="2.1" language="vi_VN" sourcelanguage="en_US">
-<context>
+  <context>
     <name>EndSession</name>
     <message>
-        <location filename="../endsession.ui" line="35"/>
-        <source>End Session</source>
-        <translation>Kết phiên</translation>
+      <location filename="../endsession.ui" line="35" />
+      <source>End Session</source>
+      <translation>Kết phiên</translation>
     </message>
     <message>
-        <location filename="../endsession.ui" line="76"/>
-        <source>What would you like to do now?</source>
-        <translation>Bạn muốn làm gì bây giờ?</translation>
+      <location filename="../endsession.ui" line="76" />
+      <source>What would you like to do now?</source>
+      <translation>Bạn muốn làm gì bây giờ?</translation>
     </message>
     <message>
-        <location filename="../endsession.ui" line="88"/>
-        <source>Power Off</source>
-        <translation>Tắt nguồn</translation>
+      <location filename="../endsession.ui" line="88" />
+      <source>Power Off</source>
+      <translation>Tắt nguồn</translation>
     </message>
     <message>
-        <location filename="../endsession.ui" line="95"/>
-        <source>Turns off your computer.</source>
-        <translation>Tắt máy tính của bạn.</translation>
+      <location filename="../endsession.ui" line="95" />
+      <source>Turns off your computer.</source>
+      <translation>Tắt máy tính của bạn.</translation>
     </message>
     <message>
-        <location filename="../endsession.ui" line="105"/>
-        <source>Reboot</source>
-        <translation>Khởi động lại</translation>
+      <location filename="../endsession.ui" line="105" />
+      <source>Reboot</source>
+      <translation>Khởi động lại</translation>
     </message>
     <message>
-        <location filename="../endsession.ui" line="112"/>
-        <source>Turns off your computer and turns it on again</source>
-        <translation>Tắt máy tính của bạn, rồi bật nó lại</translation>
+      <location filename="../endsession.ui" line="112" />
+      <source>Turns off your computer and turns it on again</source>
+      <translation>Tắt máy tính của bạn, rồi bật nó lại</translation>
     </message>
     <message>
-        <location filename="../endsession.ui" line="119"/>
-        <source>Suspend</source>
-        <translation>Tạm dừng</translation>
+      <location filename="../endsession.ui" line="119" />
+      <source>Suspend</source>
+      <translation>Tạm dừng</translation>
     </message>
     <message>
-        <location filename="../endsession.ui" line="126"/>
-        <source>Puts your computer in a low power state so it reopens quickly</source>
-        <translation>Đặt máy tính của bạn vào chế độ tiêu ít năng lượng để cho nó mở lại nhanh</translation>
+      <location filename="../endsession.ui" line="126" />
+      <source>Puts your computer in a low power state so it reopens quickly</source>
+      <translation>Đặt máy tính của bạn vào chế độ tiêu ít năng lượng để cho nó mở lại nhanh</translation>
     </message>
     <message>
-        <location filename="../endsession.ui" line="133"/>
-        <source>Exit</source>
-        <translation>Thoát</translation>
+      <location filename="../endsession.ui" line="133" />
+      <source>Exit</source>
+      <translation>Thoát</translation>
     </message>
-</context>
-<context>
+  </context>
+  <context>
     <name>OnboardingCompleteOobe</name>
     <message>
-        <location filename="../OnboardingPages/onboardingcompleteoobe.ui" line="38"/>
-        <source>That&apos;s everything!</source>
-        <translation>Đó là tất cả!</translation>
+      <location filename="../OnboardingPages/onboardingcompleteoobe.ui" line="38" />
+      <source>That's everything!</source>
+      <translation>Đó là tất cả!</translation>
     </message>
     <message>
-        <location filename="../OnboardingPages/onboardingcompleteoobe.ui" line="48"/>
-        <source>We hope you enjoy using theDesk!</source>
-        <translation>Chúng tôi hy vọng bạn thích sử dụng theDesk!</translation>
+      <location filename="../OnboardingPages/onboardingcompleteoobe.ui" line="48" />
+      <source>We hope you enjoy using theDesk!</source>
+      <translation>Chúng tôi hy vọng bạn thích sử dụng theDesk!</translation>
     </message>
     <message>
-        <location filename="../OnboardingPages/onboardingcompleteoobe.ui" line="73"/>
-        <source>Get Started</source>
-        <translation>Bắt đầu</translation>
+      <location filename="../OnboardingPages/onboardingcompleteoobe.ui" line="73" />
+      <source>Get Started</source>
+      <translation>Bắt đầu</translation>
     </message>
     <message>
-        <location filename="../OnboardingPages/onboardingcompleteoobe.cpp" line="58"/>
-        <location filename="../OnboardingPages/onboardingcompleteoobe.cpp" line="64"/>
-        <source>We hope you enjoy using %1!</source>
-        <translation>Chúng tôi hy vọng bạn thích sử dụng %1!</translation>
+      <location filename="../OnboardingPages/onboardingcompleteoobe.cpp" line="58" />
+      <location filename="../OnboardingPages/onboardingcompleteoobe.cpp" line="64" />
+      <source>We hope you enjoy using %1!</source>
+      <translation>Chúng tôi hy vọng bạn thích sử dụng %1!</translation>
     </message>
     <message>
-        <location filename="../OnboardingPages/onboardingcompleteoobe.cpp" line="58"/>
-        <location filename="../OnboardingPages/onboardingcompleteoobe.cpp" line="64"/>
-        <source>your new device</source>
-        <translation>thiết bị mới của bạn</translation>
+      <location filename="../OnboardingPages/onboardingcompleteoobe.cpp" line="58" />
+      <location filename="../OnboardingPages/onboardingcompleteoobe.cpp" line="64" />
+      <source>your new device</source>
+      <translation>thiết bị mới của bạn</translation>
     </message>
     <message>
-        <location filename="../OnboardingPages/onboardingcompleteoobe.cpp" line="77"/>
-        <source>Done</source>
-        <translation>Hoàn thành</translation>
+      <location filename="../OnboardingPages/onboardingcompleteoobe.cpp" line="77" />
+      <source>Done</source>
+      <translation>Hoàn thành</translation>
     </message>
-</context>
-<context>
+  </context>
+  <context>
     <name>OnboardingHostname</name>
     <message>
-        <location filename="../OnboardingPages/onboardinghostname.ui" line="35"/>
-        <location filename="../OnboardingPages/onboardinghostname.cpp" line="98"/>
-        <source>Device Name</source>
-        <translation>Tên thiết bị</translation>
+      <location filename="../OnboardingPages/onboardinghostname.ui" line="35" />
+      <location filename="../OnboardingPages/onboardinghostname.cpp" line="98" />
+      <source>Device Name</source>
+      <translation>Tên thiết bị</translation>
     </message>
     <message>
-        <location filename="../OnboardingPages/onboardinghostname.ui" line="59"/>
-        <source>Family PC</source>
-        <translation>PC gia đình</translation>
+      <location filename="../OnboardingPages/onboardinghostname.ui" line="59" />
+      <source>Family PC</source>
+      <translation>PC gia đình</translation>
     </message>
     <message>
-        <location filename="../OnboardingPages/onboardinghostname.ui" line="66"/>
-        <source>New Device Name</source>
-        <translation>Tên thiết bị mới</translation>
+      <location filename="../OnboardingPages/onboardinghostname.ui" line="66" />
+      <source>New Device Name</source>
+      <translation>Tên thiết bị mới</translation>
     </message>
     <message>
-        <location filename="../OnboardingPages/onboardinghostname.ui" line="73"/>
-        <source>Set a device name for this device. It&apos;s used to identify this device over the network, and will also be used as the device name over Bluetooth.</source>
-        <translation>Chọn một tên cho thiết bị này. Nó sẽ bị dùng để nhận định thiết bị này qua mạng, và cũng sử dụng cho tên thiết bị này qua Bluetooth.</translation>
+      <location filename="../OnboardingPages/onboardinghostname.ui" line="73" />
+      <source>Set a device name for this device. It's used to identify this device over the network, and will also be used as the device name over Bluetooth.</source>
+      <translation>Chọn một tên cho thiết bị này. Nó sẽ bị dùng để nhận định thiết bị này qua mạng, và cũng sử dụng cho tên thiết bị này qua Bluetooth.</translation>
     </message>
     <message>
-        <location filename="../OnboardingPages/onboardinghostname.ui" line="83"/>
-        <source>Network Compatible Name</source>
-        <translation>Tên mạng tương thích</translation>
+      <location filename="../OnboardingPages/onboardinghostname.ui" line="83" />
+      <source>Network Compatible Name</source>
+      <translation>Tên mạng tương thích</translation>
     </message>
     <message>
-        <location filename="../OnboardingPages/onboardinghostname.ui" line="158"/>
-        <source>Next</source>
-        <translation>Tiếp</translation>
+      <location filename="../OnboardingPages/onboardinghostname.ui" line="158" />
+      <source>Next</source>
+      <translation>Tiếp</translation>
     </message>
-</context>
+  </context>
 </TS>
