88a8e4e98842e9bf20a80bdbc208c37e7581d7d1
Parlance <parlance@vicr123.com>
2022-08-22 02:12:14 +0000

    Update Translations

--- boot-utilities/translations/en_GB.ts
+++ boot-utilities/translations/en_GB.ts
@@ -1,93 +1,93 @@
-<?xml version="1.0" encoding="utf-8"?>
-<!DOCTYPE TS>
+<?xml version="1.0" encoding="utf-16"?>
+<!DOCTYPE TS []>
 <TS version="2.1" language="en_GB" sourcelanguage="en_US">
-<context>
+  <context>
     <name>LanguagePopover</name>
     <message>
-        <location filename="../languagepopover.ui" line="57"/>
-        <source>Select a locale</source>
-        <translation>Select a locale</translation>
+      <location filename="../languagepopover.ui" line="57" />
+      <source>Select a locale</source>
+      <translation>Select a locale</translation>
     </message>
-</context>
-<context>
+  </context>
+  <context>
     <name>MainWindow</name>
     <message>
-        <location filename="../mainwindow.ui" line="122"/>
-        <source>Language</source>
-        <translation>Language</translation>
+      <location filename="../mainwindow.ui" line="122" />
+      <source>Language</source>
+      <translation>Language</translation>
     </message>
     <message>
-        <location filename="../mainwindow.ui" line="148"/>
-        <source>Emergency</source>
-        <translation>Emergency</translation>
+      <location filename="../mainwindow.ui" line="148" />
+      <source>Emergency</source>
+      <translation>Emergency</translation>
     </message>
     <message>
-        <location filename="../mainwindow.ui" line="180"/>
-        <source>Manage Disks</source>
-        <translation>Manage Disks</translation>
+      <location filename="../mainwindow.ui" line="180" />
+      <source>Manage Disks</source>
+      <translation>Manage Disks</translation>
     </message>
     <message>
-        <location filename="../mainwindow.ui" line="187"/>
-        <source>View Files</source>
-        <translation>View Files</translation>
+      <location filename="../mainwindow.ui" line="187" />
+      <source>View Files</source>
+      <translation>View Files</translation>
     </message>
     <message>
-        <location filename="../mainwindow.ui" line="194"/>
-        <source>Open Terminal</source>
-        <translation>Open Terminal</translation>
+      <location filename="../mainwindow.ui" line="194" />
+      <source>Open Terminal</source>
+      <translation>Open Terminal</translation>
     </message>
     <message>
-        <location filename="../mainwindow.ui" line="103"/>
-        <source>Available installation and recovery tools are shown below.</source>
-        <translation>Available installation and recovery tools are shown below.</translation>
+      <location filename="../mainwindow.ui" line="103" />
+      <source>Available installation and recovery tools are shown below.</source>
+      <translation>Available installation and recovery tools are shown below.</translation>
     </message>
     <message>
-        <location filename="../mainwindow.cpp" line="163"/>
-        <source>Install %1</source>
-        <translation>Install %1</translation>
+      <location filename="../mainwindow.cpp" line="163" />
+      <source>Install %1</source>
+      <translation>Install %1</translation>
     </message>
     <message>
-        <location filename="../mainwindow.cpp" line="161"/>
-        <source>Unknown</source>
-        <translation>Unknown</translation>
+      <location filename="../mainwindow.cpp" line="161" />
+      <source>Unknown</source>
+      <translation>Unknown</translation>
     </message>
     <message>
-        <location filename="../mainwindow.cpp" line="162"/>
-        <source>Welcome to %1!</source>
-        <translation>Welcome to %1!</translation>
+      <location filename="../mainwindow.cpp" line="162" />
+      <source>Welcome to %1!</source>
+      <translation>Welcome to %1!</translation>
     </message>
-</context>
-<context>
+  </context>
+  <context>
     <name>PowerPopover</name>
     <message>
-        <location filename="../powerpopover.ui" line="35"/>
-        <source>Power Options</source>
-        <translation>Power Options</translation>
+      <location filename="../powerpopover.ui" line="35" />
+      <source>Power Options</source>
+      <translation>Power Options</translation>
     </message>
     <message>
-        <location filename="../powerpopover.ui" line="42"/>
-        <source>What do you want to do now?</source>
-        <translation>What do you want to do now?</translation>
+      <location filename="../powerpopover.ui" line="42" />
+      <source>What do you want to do now?</source>
+      <translation>What do you want to do now?</translation>
     </message>
     <message>
-        <location filename="../powerpopover.ui" line="52"/>
-        <source>Power Off</source>
-        <translation>Power Off</translation>
+      <location filename="../powerpopover.ui" line="52" />
+      <source>Power Off</source>
+      <translation>Power Off</translation>
     </message>
     <message>
-        <location filename="../powerpopover.ui" line="59"/>
-        <source>Turns off your computer.</source>
-        <translation>Turns off your computer.</translation>
+      <location filename="../powerpopover.ui" line="59" />
+      <source>Turns off your computer.</source>
+      <translation>Turns off your computer.</translation>
     </message>
     <message>
-        <location filename="../powerpopover.ui" line="69"/>
-        <source>Reboot</source>
-        <translation>Reboot</translation>
+      <location filename="../powerpopover.ui" line="69" />
+      <source>Reboot</source>
+      <translation>Reboot</translation>
     </message>
     <message>
-        <location filename="../powerpopover.ui" line="76"/>
-        <source>Turns off your computer and turns it on again</source>
-        <translation>Turns off your computer and turns it on again</translation>
+      <location filename="../powerpopover.ui" line="76" />
+      <source>Turns off your computer and turns it on again</source>
+      <translation>Turns off your computer and turns it on again</translation>
     </message>
-</context>
+  </context>
 </TS>
--- boot-utilities/translations/vi_VN.ts
+++ boot-utilities/translations/vi_VN.ts
@@ -1,93 +1,93 @@
-<?xml version="1.0" encoding="utf-8"?>
-<!DOCTYPE TS>
+<?xml version="1.0" encoding="utf-16"?>
+<!DOCTYPE TS []>
 <TS version="2.1" language="vi_VN" sourcelanguage="en_US">
-<context>
+  <context>
     <name>LanguagePopover</name>
     <message>
-        <location filename="../languagepopover.ui" line="57"/>
-        <source>Select a locale</source>
-        <translation>Chọn một ngôn ngữ</translation>
+      <location filename="../languagepopover.ui" line="57" />
+      <source>Select a locale</source>
+      <translation>Chọn một ngôn ngữ</translation>
     </message>
-</context>
-<context>
+  </context>
+  <context>
     <name>MainWindow</name>
     <message>
-        <location filename="../mainwindow.ui" line="122"/>
-        <source>Language</source>
-        <translation>Ngôn ngữ</translation>
+      <location filename="../mainwindow.ui" line="122" />
+      <source>Language</source>
+      <translation>Ngôn ngữ</translation>
     </message>
     <message>
-        <location filename="../mainwindow.ui" line="148"/>
-        <source>Emergency</source>
-        <translation>Khẩn cấp</translation>
+      <location filename="../mainwindow.ui" line="148" />
+      <source>Emergency</source>
+      <translation>Khẩn cấp</translation>
     </message>
     <message>
-        <location filename="../mainwindow.ui" line="180"/>
-        <source>Manage Disks</source>
-        <translation>Quản lý đĩa</translation>
+      <location filename="../mainwindow.ui" line="180" />
+      <source>Manage Disks</source>
+      <translation>Quản lý đĩa</translation>
     </message>
     <message>
-        <location filename="../mainwindow.ui" line="187"/>
-        <source>View Files</source>
-        <translation>Xem tệp</translation>
+      <location filename="../mainwindow.ui" line="187" />
+      <source>View Files</source>
+      <translation>Xem tệp</translation>
     </message>
     <message>
-        <location filename="../mainwindow.ui" line="194"/>
-        <source>Open Terminal</source>
-        <translation>Mở thiết bị cuối</translation>
+      <location filename="../mainwindow.ui" line="194" />
+      <source>Open Terminal</source>
+      <translation>Mở thiết bị cuối</translation>
     </message>
     <message>
-        <location filename="../mainwindow.ui" line="103"/>
-        <source>Available installation and recovery tools are shown below.</source>
-        <translation>Các tiện ích cho cài đặt và khôi phục sẽ hiện lên dưới đây.</translation>
+      <location filename="../mainwindow.ui" line="103" />
+      <source>Available installation and recovery tools are shown below.</source>
+      <translation>Các tiện ích cho cài đặt và khôi phục sẽ hiện lên dưới đây.</translation>
     </message>
     <message>
-        <location filename="../mainwindow.cpp" line="163"/>
-        <source>Install %1</source>
-        <translation>Cài đặt %1</translation>
+      <location filename="../mainwindow.cpp" line="163" />
+      <source>Install %1</source>
+      <translation>Cài đặt %1</translation>
     </message>
     <message>
-        <location filename="../mainwindow.cpp" line="161"/>
-        <source>Unknown</source>
-        <translation>Không xác định</translation>
+      <location filename="../mainwindow.cpp" line="161" />
+      <source>Unknown</source>
+      <translation>Không xác định</translation>
     </message>
     <message>
-        <location filename="../mainwindow.cpp" line="162"/>
-        <source>Welcome to %1!</source>
-        <translation>Chào mừng bạn với %1!</translation>
+      <location filename="../mainwindow.cpp" line="162" />
+      <source>Welcome to %1!</source>
+      <translation>Chào mừng bạn với %1!</translation>
     </message>
-</context>
-<context>
+  </context>
+  <context>
     <name>PowerPopover</name>
     <message>
-        <location filename="../powerpopover.ui" line="35"/>
-        <source>Power Options</source>
-        <translation>Tùy chọn nguồn</translation>
+      <location filename="../powerpopover.ui" line="35" />
+      <source>Power Options</source>
+      <translation>Tùy chọn nguồn</translation>
     </message>
     <message>
-        <location filename="../powerpopover.ui" line="42"/>
-        <source>What do you want to do now?</source>
-        <translation>Bạn muốn làm gì bây giờ?</translation>
+      <location filename="../powerpopover.ui" line="42" />
+      <source>What do you want to do now?</source>
+      <translation>Bạn muốn làm gì bây giờ?</translation>
     </message>
     <message>
-        <location filename="../powerpopover.ui" line="52"/>
-        <source>Power Off</source>
-        <translation>Tắt nguồn</translation>
+      <location filename="../powerpopover.ui" line="52" />
+      <source>Power Off</source>
+      <translation>Tắt nguồn</translation>
     </message>
     <message>
-        <location filename="../powerpopover.ui" line="59"/>
-        <source>Turns off your computer.</source>
-        <translation>Tắt máy tính của bạn.</translation>
+      <location filename="../powerpopover.ui" line="59" />
+      <source>Turns off your computer.</source>
+      <translation>Tắt máy tính của bạn.</translation>
     </message>
     <message>
-        <location filename="../powerpopover.ui" line="69"/>
-        <source>Reboot</source>
-        <translation>Khởi động lại</translation>
+      <location filename="../powerpopover.ui" line="69" />
+      <source>Reboot</source>
+      <translation>Khởi động lại</translation>
     </message>
     <message>
-        <location filename="../powerpopover.ui" line="76"/>
-        <source>Turns off your computer and turns it on again</source>
-        <translation>Tắt máy tính của bạn, rồi bật nó lại</translation>
+      <location filename="../powerpopover.ui" line="76" />
+      <source>Turns off your computer and turns it on again</source>
+      <translation>Tắt máy tính của bạn, rồi bật nó lại</translation>
     </message>
-</context>
+  </context>
 </TS>
