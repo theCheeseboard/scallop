f770d09c9274063e087df52d609c1e1eae03c34e
Victor Tran <vicr12345@gmail.com>
2023-07-16 16:26:58 +1000

    Ensure system reset works

--- boot-utilities/translations/nl_NL.ts
+++ boot-utilities/translations/nl_NL.ts
@@ -1,51 +1,52 @@
 <?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
 <TS version="2.1" language="nl_NL">
-  <context>
+<context>
     <name>LanguagePopover</name>
     <message>
-      <translation>Kies een taal</translation>
-      <location filename="../languagepopover.ui" line="57" />
-      <location filename="../../../build-scallop-Chroot_6-Debug/boot-utilities/scallopbootutil_autogen/include/ui_languagepopover.h" line="99" />
-      <source>Select a locale</source>
+        <location filename="../languagepopover.ui" line="57"/>
+        <location filename="../../../build-scallop-Chroot_6-Debug/boot-utilities/scallopbootutil_autogen/include/ui_languagepopover.h" line="99"/>
+        <source>Select a locale</source>
+        <translation>Kies een taal</translation>
     </message>
-  </context>
-  <context>
+</context>
+<context>
     <name>MainWindow</name>
     <message>
-      <translation>Taal</translation>
-      <location filename="../mainwindow.ui" line="125" />
-      <location filename="../../../build-scallop-Chroot_6-Debug/boot-utilities/scallopbootutil_autogen/include/ui_mainwindow.h" line="248" />
-      <source>Language</source>
+        <location filename="../mainwindow.ui" line="125"/>
+        <location filename="../../../build-scallop-Chroot_6-Debug/boot-utilities/scallopbootutil_autogen/include/ui_mainwindow.h" line="248"/>
+        <source>Language</source>
+        <translation>Taal</translation>
     </message>
     <message>
-      <translation>Noodgeval</translation>
-      <location filename="../mainwindow.ui" line="151" />
-      <location filename="../../../build-scallop-Chroot_6-Debug/boot-utilities/scallopbootutil_autogen/include/ui_mainwindow.h" line="249" />
-      <source>Emergency</source>
+        <location filename="../mainwindow.ui" line="151"/>
+        <location filename="../../../build-scallop-Chroot_6-Debug/boot-utilities/scallopbootutil_autogen/include/ui_mainwindow.h" line="249"/>
+        <source>Emergency</source>
+        <translation>Noodgeval</translation>
     </message>
     <message>
-      <translation>Schijven Beheren</translation>
-      <location filename="../mainwindow.ui" line="183" />
-      <location filename="../../../build-scallop-Chroot_6-Debug/boot-utilities/scallopbootutil_autogen/include/ui_mainwindow.h" line="250" />
-      <source>Manage Disks</source>
+        <location filename="../mainwindow.ui" line="183"/>
+        <location filename="../../../build-scallop-Chroot_6-Debug/boot-utilities/scallopbootutil_autogen/include/ui_mainwindow.h" line="250"/>
+        <source>Manage Disks</source>
+        <translation>Schijven Beheren</translation>
     </message>
     <message>
-      <translation>Bestanden Weergeven</translation>
-      <location filename="../mainwindow.ui" line="190" />
-      <location filename="../../../build-scallop-Chroot_6-Debug/boot-utilities/scallopbootutil_autogen/include/ui_mainwindow.h" line="251" />
-      <source>View Files</source>
+        <location filename="../mainwindow.ui" line="190"/>
+        <location filename="../../../build-scallop-Chroot_6-Debug/boot-utilities/scallopbootutil_autogen/include/ui_mainwindow.h" line="251"/>
+        <source>View Files</source>
+        <translation>Bestanden Weergeven</translation>
     </message>
     <message>
-      <translation>Terminal Openen</translation>
-      <location filename="../mainwindow.ui" line="197" />
-      <location filename="../../../build-scallop-Chroot_6-Debug/boot-utilities/scallopbootutil_autogen/include/ui_mainwindow.h" line="252" />
-      <source>Open Terminal</source>
+        <location filename="../mainwindow.ui" line="197"/>
+        <location filename="../../../build-scallop-Chroot_6-Debug/boot-utilities/scallopbootutil_autogen/include/ui_mainwindow.h" line="252"/>
+        <source>Open Terminal</source>
+        <translation>Terminal Openen</translation>
     </message>
     <message>
-      <translation>Hieronder staan de beschikbare installatie- en hersteltools.</translation>
-      <location filename="../mainwindow.ui" line="106" />
-      <location filename="../../../build-scallop-Chroot_6-Debug/boot-utilities/scallopbootutil_autogen/include/ui_mainwindow.h" line="247" />
-      <source>Available installation and recovery tools are shown below.</source>
+        <location filename="../mainwindow.ui" line="106"/>
+        <location filename="../../../build-scallop-Chroot_6-Debug/boot-utilities/scallopbootutil_autogen/include/ui_mainwindow.h" line="247"/>
+        <source>Available installation and recovery tools are shown below.</source>
+        <translation>Hieronder staan de beschikbare installatie- en hersteltools.</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="159"/>
@@ -62,45 +63,44 @@
         <source>Welcome to %1!</source>
         <translation type="unfinished"></translation>
     </message>
-  </context>
-  <context>
+</context>
+<context>
     <name>PowerPopover</name>
     <message>
-      <translation></translation>
-      <location filename="../powerpopover.ui" line="35" />
-      <location filename="../../../build-scallop-Chroot_6-Debug/boot-utilities/scallopbootutil_autogen/include/ui_powerpopover.h" line="94" />
-      <source>Power Options</source>
+        <location filename="../powerpopover.ui" line="35"/>
+        <location filename="../../../build-scallop-Chroot_6-Debug/boot-utilities/scallopbootutil_autogen/include/ui_powerpopover.h" line="94"/>
+        <source>Power Options</source>
+        <translation></translation>
     </message>
     <message>
-      <translation>Wat wil je doen?</translation>
-      <location filename="../powerpopover.ui" line="42" />
-      <location filename="../../../build-scallop-Chroot_6-Debug/boot-utilities/scallopbootutil_autogen/include/ui_powerpopover.h" line="95" />
-      <source>What do you want to do now?</source>
+        <location filename="../powerpopover.ui" line="42"/>
+        <location filename="../../../build-scallop-Chroot_6-Debug/boot-utilities/scallopbootutil_autogen/include/ui_powerpopover.h" line="95"/>
+        <source>What do you want to do now?</source>
+        <translation>Wat wil je doen?</translation>
     </message>
     <message>
-      <translation>Afsluiten</translation>
-      <location filename="../powerpopover.ui" line="52" />
-      <location filename="../../../build-scallop-Chroot_6-Debug/boot-utilities/scallopbootutil_autogen/include/ui_powerpopover.h" line="96" />
-      <source>Power Off</source>
+        <location filename="../powerpopover.ui" line="52"/>
+        <location filename="../../../build-scallop-Chroot_6-Debug/boot-utilities/scallopbootutil_autogen/include/ui_powerpopover.h" line="96"/>
+        <source>Power Off</source>
+        <translation>Afsluiten</translation>
     </message>
     <message>
-      <translation>Sluit je computer af.</translation>
-      <location filename="../powerpopover.ui" line="59" />
-      <location filename="../../../build-scallop-Chroot_6-Debug/boot-utilities/scallopbootutil_autogen/include/ui_powerpopover.h" line="97" />
-      <source>Turns off your computer.</source>
+        <location filename="../powerpopover.ui" line="59"/>
+        <location filename="../../../build-scallop-Chroot_6-Debug/boot-utilities/scallopbootutil_autogen/include/ui_powerpopover.h" line="97"/>
+        <source>Turns off your computer.</source>
+        <translation>Sluit je computer af.</translation>
     </message>
     <message>
-      <translation>Herstarten</translation>
-      <location filename="../powerpopover.ui" line="69" />
-      <location filename="../../../build-scallop-Chroot_6-Debug/boot-utilities/scallopbootutil_autogen/include/ui_powerpopover.h" line="98" />
-      <source>Reboot</source>
+        <location filename="../powerpopover.ui" line="69"/>
+        <location filename="../../../build-scallop-Chroot_6-Debug/boot-utilities/scallopbootutil_autogen/include/ui_powerpopover.h" line="98"/>
+        <source>Reboot</source>
+        <translation>Herstarten</translation>
     </message>
     <message>
-      <translation>Zet je computer uit en weer aan</translation>
-      <location filename="../powerpopover.ui" line="76" />
-      <location filename="../../../build-scallop-Chroot_6-Debug/boot-utilities/scallopbootutil_autogen/include/ui_powerpopover.h" line="99" />
-      <source>Turns off your computer and turns it on again</source>
+        <location filename="../powerpopover.ui" line="76"/>
+        <location filename="../../../build-scallop-Chroot_6-Debug/boot-utilities/scallopbootutil_autogen/include/ui_powerpopover.h" line="99"/>
+        <source>Turns off your computer and turns it on again</source>
+        <translation>Zet je computer uit en weer aan</translation>
     </message>
-  </context>
+</context>
 </TS>
-
