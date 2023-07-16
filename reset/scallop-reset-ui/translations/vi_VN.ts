7776386c48c5943be2051cc1ecd364a7ba3bf408
Parlance <parlance@vicr123.com>
2023-07-16 11:07:00 +0000

    Update Translations - vi_VN
    Co-Authored-By: Victor Tran <vicr12345@gmail.com>

---
+++ reset/scallop-reset-ui/translations/vi_VN.ts
@@ -0,0 +1,123 @@
+﻿<?xml version="1.0" encoding="utf-8"?>
+<TS version="2.1" language="vi_VN">
+  <context>
+    <name>DownloadProgress</name>
+    <message>
+      <translation>Đang chuẩn bị đặt lại</translation>
+      <location filename="../downloadprogress.ui" line="58" />
+      <location filename="../../../../build-scallop-Chroot_6-Debug/reset/scallop-reset-ui/scallopresetui_autogen/include/ui_downloadprogress.h" line="122" />
+      <source>Preparing to Reset</source>
+    </message>
+    <message>
+      <translation>Chúng tôi đang chuẩn bị thiết bị này để bị đặt lại. Vùi lòng giữ kết nối với Internet. Việc này sẽ tốn một lúc.</translation>
+      <location filename="../downloadprogress.ui" line="73" />
+      <location filename="../../../../build-scallop-Chroot_6-Debug/reset/scallop-reset-ui/scallopresetui_autogen/include/ui_downloadprogress.h" line="123" />
+      <source>We're preparing this device to be reset. Stay connected to the Internet. This will take a while.</source>
+    </message>
+    <message>
+      <translation>Không thể đạt lại</translation>
+      <location filename="../downloadprogress.cpp" line="61" />
+      <source>Reset Failed</source>
+    </message>
+    <message>
+      <translation>Chúng tôi không có thể tải xuống ảnh khôi phục. Vùi lòng thử lại sau.</translation>
+      <location filename="../downloadprogress.cpp" line="61" />
+      <source>We weren't able to download the recovery image. Please try again later.</source>
+    </message>
+    <message>
+      <translation>Đã tải xuống %1 trên %2</translation>
+      <location filename="../downloadprogress.cpp" line="68" />
+      <source>Downloaded %1 of %2</source>
+    </message>
+  </context>
+  <context>
+    <name>FinalResetPopover</name>
+    <message>
+      <translation>SẴN SÀNG CHƯA?</translation>
+      <location filename="../finalresetpopover.ui" line="51" />
+      <location filename="../../../../build-scallop-Chroot_6-Debug/reset/scallop-reset-ui/scallopresetui_autogen/include/ui_finalresetpopover.h" line="99" />
+      <source>THIS IS IT</source>
+    </message>
+    <message>
+      <translation>Không thể rút lại! Sau khi thiết bị này bị đặt lại, các dự liệu của bạn sẽ bị mất mãi mãi.</translation>
+      <location filename="../finalresetpopover.ui" line="58" />
+      <location filename="../../../../build-scallop-Chroot_6-Debug/reset/scallop-reset-ui/scallopresetui_autogen/include/ui_finalresetpopover.h" line="100" />
+      <source>There's no going back after this. Once this device is reset, all your data is gone forever.</source>
+    </message>
+    <message>
+      <translation>Khởi động lại và đặt lại thiết bị này</translation>
+      <location filename="../finalresetpopover.ui" line="68" />
+      <location filename="../../../../build-scallop-Chroot_6-Debug/reset/scallop-reset-ui/scallopresetui_autogen/include/ui_finalresetpopover.h" line="101" />
+      <source>Reboot and reset this device</source>
+    </message>
+    <message>
+      <translation>Đặt lại thiết bị này</translation>
+      <location filename="../finalresetpopover.cpp" line="28" />
+      <source>Reset this device</source>
+    </message>
+  </context>
+  <context>
+    <name>MainWindow</name>
+    <message>
+      <translation>Đặt lại thiết bị này</translation>
+      <location filename="../mainwindow.ui" line="14" />
+      <location filename="../mainwindow.ui" line="125" />
+      <location filename="../../../../build-scallop-Chroot_6-Debug/reset/scallop-reset-ui/scallopresetui_autogen/include/ui_mainwindow.h" line="154" />
+      <location filename="../../../../build-scallop-Chroot_6-Debug/reset/scallop-reset-ui/scallopresetui_autogen/include/ui_mainwindow.h" line="156" />
+      <source>Reset this device</source>
+    </message>
+    <message>
+      <translation>Có, đặt lại thiết bị này</translation>
+      <location filename="../mainwindow.ui" line="154" />
+      <location filename="../../../../build-scallop-Chroot_6-Debug/reset/scallop-reset-ui/scallopresetui_autogen/include/ui_mainwindow.h" line="157" />
+      <source>Yes, reset this device</source>
+    </message>
+    <message>
+      <translation>Các dự liệu sẽ bị xóa và thiết bị này sẽ khởi động lại.</translation>
+      <location filename="../mainwindow.ui" line="157" />
+      <location filename="../../../../build-scallop-Chroot_6-Debug/reset/scallop-reset-ui/scallopresetui_autogen/include/ui_mainwindow.h" line="158" />
+      <source>All data will be deleted and this device will restart.</source>
+    </message>
+    <message>
+      <translation>Không, đừng đất lại thiết bị này</translation>
+      <location filename="../mainwindow.ui" line="164" />
+      <location filename="../../../../build-scallop-Chroot_6-Debug/reset/scallop-reset-ui/scallopresetui_autogen/include/ui_mainwindow.h" line="159" />
+      <source>No, don't reset this device</source>
+    </message>
+    <message>
+      <translation>Dữ liệu của bạn sẽ được giữ lại.</translation>
+      <location filename="../mainwindow.ui" line="167" />
+      <location filename="../../../../build-scallop-Chroot_6-Debug/reset/scallop-reset-ui/scallopresetui_autogen/include/ui_mainwindow.h" line="160" />
+      <source>Your data will be preserved.</source>
+    </message>
+    <message>
+      <translation>Không xác định</translation>
+      <location filename="../mainwindow.cpp" line="96" />
+      <location filename="../mainwindow.cpp" line="98" />
+      <source>Unknown</source>
+    </message>
+    <message>
+      <translation></translation>
+      <location filename="../mainwindow.cpp" line="101" />
+      <source>**BUCKLE UP!**
+
+Resetting this device will erase all personal data **for all accounts.** This includes:
+- Documents
+- Pictures
+- Installed apps
+- Any changed settings
+
+**READY TO DO THIS?**
+
+To get the ball rolling, we'll restart this device and start removing all the data on it. Once the reset is complete, a new copy of %1 will be installed. This process can take a while.</source>
+    </message>
+  </context>
+  <context>
+    <name>main</name>
+    <message>
+      <translation></translation>
+      <location filename="../main.cpp" line="31" />
+      <source>System Configuration Tool</source>
+    </message>
+  </context>
+</TS>
